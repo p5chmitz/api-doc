95be168a0113050c5fb3b5665d63fba0c8c224ec
p5chmitz <petermschmitz@gmail.com>
2025-04-11 20:21:04 -0700

    Comments out older migration experiments

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,10 +1,10 @@
 pub use sea_orm_migration::prelude::*;
 
-mod m20220101_000001_create_table;
-mod m20250324_012019_add_test_column_to_table;
-mod m20250325_000311_fix_user_table;
-mod m20250327_025117_add_patient_schema;
-mod m20250401_031514_add_patient_metadata;
+//mod m20220101_000001_create_table;
+//mod m20250324_012019_add_test_column_to_table;
+//mod m20250325_000311_fix_user_table;
+//mod m20250327_025117_add_patient_schema;
+//mod m20250401_031514_add_patient_metadata;
 mod m20250407_035528_create_base_schema;
 
 pub struct Migrator;
@@ -13,11 +13,11 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            Box::new(m20220101_000001_create_table::Migration),
-            Box::new(m20250324_012019_add_test_column_to_table::Migration),
-            Box::new(m20250325_000311_fix_user_table::Migration),
-            Box::new(m20250327_025117_add_patient_schema::Migration),
-            Box::new(m20250401_031514_add_patient_metadata::Migration),
+            //Box::new(m20220101_000001_create_table::Migration),
+            //Box::new(m20250324_012019_add_test_column_to_table::Migration),
+            //Box::new(m20250325_000311_fix_user_table::Migration),
+            //Box::new(m20250327_025117_add_patient_schema::Migration),
+            //Box::new(m20250401_031514_add_patient_metadata::Migration),
             Box::new(m20250407_035528_create_base_schema::Migration),
         ]
     }
