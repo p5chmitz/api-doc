b3783f46b269fadbef7d476bf6b52929415163b7
p5chmitz <petermschmitz@gmail.com>
2025-04-25 13:04:27 -0700

    Adds newest schema config

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -5,7 +5,8 @@ pub use sea_orm_migration::prelude::*;
 //mod m20250325_000311_fix_user_table;
 //mod m20250327_025117_add_patient_schema;
 //mod m20250401_031514_add_patient_metadata;
-mod m20250407_035528_create_base_schema;
+//mod m20250407_035528_create_base_schema;
+mod m20250424_233306_create_base_schema;
 
 pub struct Migrator;
 
@@ -13,12 +14,7 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            //Box::new(m20220101_000001_create_table::Migration),
-            //Box::new(m20250324_012019_add_test_column_to_table::Migration),
-            //Box::new(m20250325_000311_fix_user_table::Migration),
-            //Box::new(m20250327_025117_add_patient_schema::Migration),
-            //Box::new(m20250401_031514_add_patient_metadata::Migration),
-            Box::new(m20250407_035528_create_base_schema::Migration),
+            Box::new(m20250424_233306_create_base_schema::Migration),
         ]
     }
 }
